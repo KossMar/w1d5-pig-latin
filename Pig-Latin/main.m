68b761bd07f79f9a65a2d381d9a3f0dbb7a637bb
Mar Koss <kossmar2@gmail.com>
2017-10-07 15:56:07 -0700

    recognizes capitalization and can properly return a capitalized pig-latinized word

--- Pig-Latin/main.m
+++ Pig-Latin/main.m
@@ -11,24 +11,44 @@
 @implementation NSString (moreMeth)
 
 -(NSString *)stringByPigLatinization {
-    NSCharacterSet *vowelSet = [NSCharacterSet characterSetWithCharactersInString:@"aeiuoy"];
-    NSCharacterSet *consonantSet = [NSCharacterSet characterSetWithCharactersInString: @"bcdfghjklmnpqrstvwxz"];
+    
+    NSCharacterSet *vowelSet = [NSCharacterSet characterSetWithCharactersInString: @"aeiuoAEIOU"];
+    NSCharacterSet *uppercase = [NSCharacterSet uppercaseLetterCharacterSet];
     NSArray *stringToArray = [self componentsSeparatedByString:@" "];
     NSMutableArray *latinizedArr = [NSMutableArray new];
     
     for (NSString *b in stringToArray){
-//        set range to length of input string
-        NSRange searchRange = NSMakeRange(0, [b length]-1);
-//        using vowelSet, iterate through input string to find first instance of character match. return range information (aka location of matching character in input string
-        NSRange foundRange = [b rangeOfCharacterFromSet:vowelSet options:0 range:searchRange];
+        if (b.length > 1) {
+            //        set range to length of input string
+            NSRange searchRange = NSMakeRange(0, [b length]);
+            //        using vowelSet, iterate through input string to find first instance of character match. return range information (aka location of matching character in input string
+            NSRange foundRange = [b rangeOfCharacterFromSet:vowelSet options:0 range:searchRange];
+            NSRange capital = [b rangeOfCharacterFromSet:uppercase];
+            
+            NSString *consonant = [b substringToIndex:foundRange.location];
+            BOOL track = NO;
+            if (capital.location == 0) {
+                track = YES;
+            }
+            
+            consonant = [consonant lowercaseString];
+            consonant = [consonant stringByAppendingString:@"ay"];
+            NSString *bSliced = [b substringFromIndex:foundRange.location];
+            bSliced = [bSliced lowercaseString];
+            NSString *latinizedStr = [bSliced stringByAppendingString:consonant];
+            
+            if (track == YES) {
+                latinizedStr = [latinizedStr capitalizedStringWithLocale:nil];
+            }
+            [latinizedArr addObject:latinizedStr];
+        }
         
-        NSString *consonant = [b substringToIndex:foundRange.location];
-        consonant = [consonant stringByAppendingString:@"ay"];
-        NSString *bSliced = [b substringFromIndex:foundRange.location];
-        NSString *latinizedStr = [bSliced stringByAppendingString:consonant];
-        [latinizedArr addObject:latinizedStr];
+        else {
+            [latinizedArr addObject:b];
+            continue;
+        }
     }
-        
+    
     
              NSString *pigLatin = [latinizedArr componentsJoinedByString:@" "];
     return pigLatin;
@@ -42,6 +62,11 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
 
+        
+        BOOL programRun = YES;
+        
+        while (programRun == YES) {
+            
         NSLog(@"Input a sentence");
         
         char str[100];
@@ -52,6 +77,7 @@ int main(int argc, const char * argv[]) {
         inputString = [inputString stringByPigLatinization];
         
         NSLog(@"%@", inputString);
+        }
         
 //        NSString *url = @"http://www.sitename.com";
 //        NSRange range = NSMakeRange(7, [url length] - 7);
